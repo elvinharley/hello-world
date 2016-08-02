e4d151904b0e450de72305713c1a0f007f168027
elvinharley <engr.aquino@yahoo.com>
2016-08-02 16:56:34 +0800

    created object instance

--- Integration Library/src/test/synchrony/integration/connectors/TestDisk.java
+++ Integration Library/src/test/synchrony/integration/connectors/TestDisk.java
@@ -1,9 +1,11 @@
 package test.synchrony.integration.connectors;
 
+import synchrony.integration.connectors.Disk;
+
 public class TestDisk {
 
 	public static void main(String[] args) {
-		
+		Disk disk = new Disk();
 
 	}
 
